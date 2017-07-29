0c67667511ec91ac98712c790f00c48d598684db
franpriggio <francopriggione@gmail.com>
2017-07-29 12:00:36 -0200

    subtitleReader

---
+++ src/prototypes/lesson1/lesson1/Assets/SD/Gameplay/Experience/MediaManager/MediaManager.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Threading;
+using UnityEngine.UI;
+using System.Diagnostics;
+
+public class MediaManager : MonoBehaviour {
+
+    private VRExperience experience = null;
+    
+    [SerializeField] private MediaPlayerCtrl media;
+    [SerializeField] private AudioSource audioLeft;
+    [SerializeField] private AudioSource audioRight;
+    [SerializeField] private MediaManagerData data;
+    [SerializeField] private VRGameMenu menu;
+    private SubtitleReader subReader;
+    //[SerializeField] private string SubtitleText;
+
+    private AudioSource sfx;
+    private Stopwatch stopwatch;
+	[SerializeField] public GUIText theGuiText;
+
+    // Use this for initialization
+    void Start ()
+    {
+		audioLeft = new AudioSource ();
+        experience = VRExperience.Instance;
+
+        if (audioLeft != null)
+        {
+            audioLeft.volume = experience.GetConfigurationValue<float>(data.videoVolumeConfigValue);
+            audioLeft.Play();
+        }
+
+        if (audioRight != null)
+        {
+            audioRight.volume = experience.GetConfigurationValue<float>(data.videoVolumeConfigValue);
+            audioRight.Play();
+        }
+
+        if (data.audioAssetKey != null)
+        {
+            sfx = gameObject.AddComponent<AudioSource>();
+
+            sfx.clip = Resources.Load<AudioClip>(data.audioAssetKey);
+           // sfx.volume = experience.GetConfigurationValue<float>(data.audioVolumeConfigValue);
+            sfx.loop = false;
+            sfx.Play();
+        }
+
+        menu.OnMenuShow += PauseMedia;
+        menu.OnMenuHide += ResumeMedia;
+        media.OnEnd += ManagerVideo;
+        media.Play();
+    }
+
+    void Awake()
+    {
+        if (media == null)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            subReader = new SubtitleReader();
+            media = FindObjectOfType<MediaPlayerCtrl>();
+            if (media == null)
+                throw new UnityException("No Media Player Ctrl object in scene");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        int seconds = (int)stopwatch.ElapsedMilliseconds;
+        // search if duration is in last subtitle second (in miliseconds)
+        // PauseMedia();
+		UnityEngine.Debug.Log("seconds: " + seconds);
+
+		theGuiText.text = subReader.ReadSubtitleLine(seconds);
+
+	/*	if (!theGuiText.text.Equals ("")) {
+			PauseMedia ();
+			//TODO: reproducir audio de subtitulo
+			Thread.Sleep(2000);
+			ResumeMedia();
+		}*/
+
+    }
+
+    private void PauseMedia()
+    {
+        if (data.audioAssetKey != null)
+        {
+            sfx.Pause();
+        }
+
+        if (audioLeft != null)
+        {
+            audioLeft.Pause();
+        }
+
+        if (audioRight)
+        {
+            audioRight.Pause();
+        }
+
+        media.Pause();
+    }
+
+    private void ResumeMedia()
+    {
+        if (data.audioAssetKey != null)
+        {
+            sfx.UnPause();
+        }
+
+        if (audioLeft != null)
+        {
+            audioLeft.UnPause();
+        }
+
+        if (audioRight)
+        {
+            audioRight.UnPause();
+        }
+
+        media.Play();
+    }
+
+    [System.Serializable]
+    public struct MediaManagerData
+    {
+        [SerializeField] public string audioAssetKey;
+        [SerializeField] public string audioVolumeConfigValue;
+        [SerializeField] public string videoAssetKey;
+        [SerializeField] public string videoVolumeConfigValue;
+    }
+
+    private void FinishExperience()
+    {
+        //Debug.Log("Llegue a FinishExperience");
+        experience.BackToMainMenu();
+    }
+
+    private void ManagerVideo()
+    {
+        string videoName=experience.NextVideo();
+        stopwatch.Reset();
+        if (!videoName.Equals("End"))
+        {
+            media.UnLoad();
+            media.Load(videoName);
+            media.Play();
+        }
+        else
+        {
+            FinishExperience();
+        }
+     
+    }
+}
---
+++ src/prototypes/lesson1/lesson1/Assets/SD/Gameplay/Experience/MediaManager/SubtitleReader.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class SubtitleReader : MonoBehaviour
+{ 
+    private Hashtable subtitlesLastSeconds;
+    private Hashtable subtitlesText;
+    public Text textObject;
+
+    public SubtitleReader() {
+        subtitlesLastSeconds = new Hashtable();
+        subtitlesText = new Hashtable();
+        FileReader();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Awake()
+    {
+      textObject = GetComponent<UnityEngine.UI.Text>();
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+
+    }
+
+    public void FileReader()
+    {
+        StreamReader subtitles = new StreamReader("Assets/TextFiles/Lesson01-01.txt");
+        int lineCounter = 0;
+        //guardo todos los ultimos segundos de los subtitulos y su texto
+        while (!subtitles.EndOfStream)
+        {
+            string line = subtitles.ReadLine();
+            int firstBracket = line.IndexOf("[") + 1;
+            int lastSquareBracket = line.IndexOf("]") - 1;
+            int lastInterval = int.Parse(line.Substring(firstBracket, lastSquareBracket));
+
+            // Add to hasmap last second interval. 
+            subtitlesLastSeconds.Add(lineCounter, lastInterval);
+
+            //agregar texto a otro hashMap
+            int parent = line.IndexOf("]")+1;
+            int dash = line.IndexOf("|");
+            int between = dash - parent;
+            string subtitleText = line.Substring(parent, between);
+
+            subtitlesText.Add(lineCounter, subtitleText);
+            lineCounter++;
+        }
+        subtitles.Close();
+    }
+
+    public string ReadSubtitleLine(long duration)
+    {
+        string subToReturn = "";
+        ICollection hashValuesLast = subtitlesLastSeconds.Values;
+        int howMany = hashValuesLast.Count;
+        int[] lastSeconds = new int[howMany];
+        hashValuesLast.CopyTo(lastSeconds, 0);
+
+        ICollection hashValuesSubs = subtitlesText.Values;
+        string[] subs = new string[howMany];
+        hashValuesSubs.CopyTo(subs, 0);
+		//nls
+        // search if duration is in last subtitle second of the hash
+        for (int i = 0; i < howMany ; i++)
+        {
+            
+            if (lastSeconds[i] < duration )
+            {
+                //  UnityEngine.Debug.Log("lastSeconds[i] : " + lastSeconds[i]);
+                //  UnityEngine.Debug.Log("entro: ");
+                //devuelvo subtitulo
+                subToReturn = subs[i];
+            }
+        }
+
+        return subToReturn;
+    }
+}
