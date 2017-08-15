ea09630d373f4847ffcff13494890cae9466cdc1
tinchoola <tinchoola@gmail.com>
2017-08-15 19:45:10 -0300

    subtitulo fijo

--- src/prototypes/lesson1/lesson1/Assets/SD/Gameplay/TextManager/LoadPanel.cs
+++ src/prototypes/lesson1/lesson1/Assets/SD/Gameplay/TextManager/LoadPanel.cs
@@ -3,51 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LoadPanel : MonoBehaviour {
+public class LoadPanel : MonoBehaviour
+{
 
-    [SerializeField] TextMesh contenido;
-	[SerializeField] TextMesh subtitle;
+    [SerializeField] TextMesh info;
+    [SerializeField] TextMesh subtitle;
     [SerializeField] TextMesh sub;
     private string aux;
-    private string subAnt;
-    private string[] arrayCont;
+    //private string subAnt;
+    private string[] arrayText;
     private int count = 0;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         aux = subtitle.text;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+    // Update is called once per frame
+    void Update()
+    {
+
         if (!subtitle.text.Equals(aux) && !subtitle.text.Equals(""))
         {
             aux = subtitle.text;
             sub.text = subtitle.text;
             if (count == 0)
             {
-                subAnt = aux;
+                info.text = sub.text;
                 count++;
             }
-            else if (count == 1)
+            else
             {
-                contenido.text = subAnt;
-                subAnt = sub.text;
-                count++;
+                info.text = info.text + "\n" + sub.text;
+                //count++;
             }
-            else if (count > 1 && count < 5)
-            {
-                contenido.text = subAnt + "\n" + contenido.text;
-                subAnt = sub.text;
-                count++;
-            }
-            else if (count == 5)
-            {
-                arrayCont = contenido.text.Split('\n');
-                contenido.text = subAnt + "\n" + arrayCont[0] + "\n" + arrayCont[1];
-                subAnt = sub.text;
-            }
+            //if (count == 4)
+            //{
+            //    arrayText = info.text.Split('\n');
+            //    for (int i = 0; i < count; i++)
+            //    {
+            //        for (int j = 0; j < count; j++)
+            //        {
+            //            if (i == j)
+            //            { info.text = "<color=#0000ffff>" + arrayText[j] + "</color>"; }
+            //            else
+            //            { info.text = info.text + arrayText[j]; }
+            //        }
+            //        info.text = "";
+            //    }
+            //}
+
+
+            //if (count == 0)
+            //{
+            //    subAnt = aux;
+            //    count++;
+            //}
+            //else if (count == 1)
+            //{
+            //    contenido.text = subAnt;
+            //    subAnt = sub.text;
+            //    count++;
+            //}
+            //else if (count > 1 && count < 5)
+            //{
+            //    contenido.text = subAnt + "\n" + contenido.text;
+            //    subAnt = sub.text;
+            //    count++;
+            //}
+            //else if (count == 5)
+            //{
+            //    arrayCont = contenido.text.Split('\n');
+            //    contenido.text = subAnt + "\n" + arrayCont[0] + "\n" + arrayCont[1];
+            //    subAnt = sub.text;
+            //}
         }
-	}
+    }
 }
---
+++ src/prototypes/lesson1/lesson1/Assets/SD/Gameplay/TextManager/LoadSubtitlePanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSubtitlePanel : MonoBehaviour {
+
+    [SerializeField] TextMesh subtitle;
+    [SerializeField] TextMesh sub;
+    [SerializeField] GameObject panelExt;
+    [SerializeField] GameObject textInfo;
+    private MeshRenderer meshPanel;
+    private MeshRenderer meshTextInfo;
+    private string aux;
+    private int count; 
+
+    // Use this for initialization
+    void Start()
+    {
+        aux = subtitle.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!subtitle.text.Equals(aux) && !subtitle.text.Equals(""))
+        {
+            aux = subtitle.text;
+            sub.text = subtitle.text;
+            count++;
+            if (count == 4)
+            {
+                ActiveMeshRenderer(meshPanel, panelExt, true);
+                ActiveMeshRenderer(meshTextInfo, textInfo, true);
+            }
+        }
+    }
+
+    private void ActiveMeshRenderer(MeshRenderer mesh, GameObject gameObj, bool v)
+    {
+        mesh = gameObj.GetComponent<MeshRenderer>();
+        mesh.enabled = v;
+    }
+}
