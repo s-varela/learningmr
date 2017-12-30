fe0ed10b920066a587b2c3791f707035d490441f
martinmazur <mrtn.mazur@gmail.com>
2017-12-29 21:13:37 -0300

    hint

---
+++ src/prototypes/lesson1/lesson1/Assets/Media/EasyMovieTexture/Scripts/HintSkipPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRStandardAssets.Utils;
+
+public class HintSkipPanel : MonoBehaviour {
+
+	[SerializeField] private VRUIAnimationClick btnHint;
+	[SerializeField] private VRUIAnimationClick btnSkip;
+
+	[SerializeField] private MediaManager mediaManager;
+
+    // Use this for initialization
+    void Start () {
+
+		if(btnHint != null)
+		{
+            btnHint.OnAnimationComplete += ButtonHintOnClick;
+		}
+		if (btnSkip != null) 
+		{
+            btnSkip.OnAnimationComplete += ButtonSkipOnClick;
+		}
+
+	}
+	
+	void Update () {
+
+	}
+
+	private void ButtonHintOnClick()
+	{
+		mediaManager.GiveHint();
+    }
+
+    private void ButtonSkipOnClick()
+    {
+		mediaManager.ExecuteSkip();
+    }
+
+}
